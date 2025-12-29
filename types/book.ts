1bc6eb461aff849f6a1258d77e9bf0e297ec64a5
Katlyn Stoll <katlynstoll@gmail.com>
2025-12-29 18:52:42 -0500

    inital commit

---
+++ types/book.ts
@@ -0,0 +1,8 @@
+export type Book = {
+  id: string;
+  title: string;
+  author: string;
+  format: "physical" | "ebook" | "audio";
+  status: "unread" | "reading" | "read";
+  moods: string[]; // max 3 moods for MVP
+};
